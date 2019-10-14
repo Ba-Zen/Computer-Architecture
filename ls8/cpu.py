a4f4bdb44b44a68b32c1d2179e2bcf637fce5792
Ba-Zen <bazenatlas@gmail.com>
2019-10-14 18:59:29 -0400

    day one complete

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,12 +2,28 @@
 
 import sys
 
+HLT = 0b00000001
+LDI = 0b10000010
+PRN = 0b01000111
+
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.PC = self.reg[0]
+
+    def __repr__(self):
+        return f"RAM: {self.ram} \n Register: {self.ram}"
+
+    def ram_read(self, address):
+        return self.ram[address]
+
+    def ram_write(self, value, address):
+        self.ram[address] = value
 
     def load(self):
         """Load a program into memory."""
@@ -18,25 +34,26 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
+        print(self.ram)
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -48,8 +65,8 @@ class CPU:
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.pc,
-            #self.fl,
-            #self.ie,
+            # self.fl,
+            # self.ie,
             self.ram_read(self.pc),
             self.ram_read(self.pc + 1),
             self.ram_read(self.pc + 2)
@@ -62,4 +79,33 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        operand_a = self.ram[self.PC + 1]
+        operand_b = self.ram[self.PC + 2]
+
+        print(f"Operand A: {operand_a} Operand B: {operand_b}")
+
+        running = True
+
+        while running:
+            IR = self.ram[self.PC]
+            print(f"Current IR: {IR}, Current PC: {self.PC}")
+
+            if IR == HLT:
+                running = False
+
+            elif IR == LDI:
+                self.reg[operand_a] = operand_b
+                self.PC += 2
+
+            elif IR == PRN:
+                print(self.reg[operand_a])
+                self.PC += 1
+
+            else:
+                print(f"Unknown command: {IR}")
+                sys.exit(1)
+
+            self.PC += 1
+
+
+test = CPU()
